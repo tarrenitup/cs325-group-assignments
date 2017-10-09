f1913033a1d4483379ffb28e32da2c3fa5c47690
Tarren Engberg <tarren@outlook.com>
2017-10-09 15:34:45 -0700

    First commit with a small amount of code for writing the output.txt file.

---
+++ GA1/select.py
@@ -0,0 +1,15 @@
+# Read input.txt and store in vars k, m & n.
+
+# handle m = 1
+
+# handle n = 1
+
+# algorithm to find kth element of the m files.
+
+# write output number to output.txt
+output = 'none'
+
+f = open('output.txt','w')
+a = input(output)
+f.write('answer:'+str(a))
+f.close()
